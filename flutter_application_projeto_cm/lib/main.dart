f38ad67d0f14304282698d0b19f2fdf0ae773b9c
SamuelGitHub1 <127244942+SamuelGitHub1@users.noreply.github.com>
2024-05-21 21:07:35 +0100

    main page

--- flutter_application_projeto_cm/lib/main.dart
+++ flutter_application_projeto_cm/lib/main.dart
@@ -4,117 +4,139 @@ import 'package:flutter_application_projeto_cm/minigames_page/minigamesMenu.dart
 import 'package:flutter_application_projeto_cm/settings_page/settings_page.dart';
 import 'package:flutter_application_projeto_cm/shop_page/shop.dart';
 
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:const HomePage(),
+      home: HomeScreen(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
+      backgroundColor: Colors.lightBlue[50],
+      body: SafeArea(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Row(
+            // Top row with icons and currency
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Text(
-                    '300',
-                    style: TextStyle(color: Colors.black),
-                  ),
+                IconButton(
+                  icon: Icon(Icons.store),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ShopApp()));
+                  },
                 ),
-                SizedBox(width: 5),
-                Icon(Icons.monetization_on),
+                Row(
+                  children: [
+                    Icon(Icons.monetization_on),
+                    Text('300'),
+                  ],
+                ),
+                IconButton(
+                  icon: Icon(Icons.person),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.settings),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
+                  },
+                ),
               ],
             ),
-            IconButton(
-              icon:const Icon(Icons.account_circle),
-              onPressed: () {
-                
-              },
+            // Middle part with ghost and window
+            Column(
+              children: [
+                Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Positioned(
+                      top: -100,
+                      left: 120,
+                      child: Container(
+                        width: 300,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/windows/window1.png'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 350,
+                      height: 350,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('colors/color_white.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            IconButton(
-              icon: const Icon(Icons.settings),
-              onPressed: () {
-                Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const SettingsPage()),
-                    );
-              },
+            // Bottom part with various icons
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: GestureDetector(
+                        child: Image.asset("/lollipop_icon.png"),
+                        onTap: () {
+                          // Adicione aqui o comportamento desejado ao clicar no pirulito
+                        },
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20.0),
+                      child: GestureDetector(
+                        child: Image.asset("/shower_icon.png"),
+                        onTap: () {
+                          // Adicione aqui o comportamento desejado ao clicar no chuveiro
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      child: Image.asset("/game_controller.png"),
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => MinigamesApp()));
+                      },
+                    ),
+                    SizedBox(width: 40), // Ajusta a distância entre os ícones
+                    GestureDetector(
+                      child: Image.asset("/customize_icon.png"),
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Customize()));
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-           Expanded(
-            child: Center(
-              child: Image.asset(
-                "/colors/color_white.png",
-  
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.brush),
-                  iconSize: 50,
-                  onPressed: () { 
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) =>const Customize()),
-                    );
-                  },
-                ),
-                IconButton(
-                  icon:const Icon(Icons.videogame_asset),
-                  iconSize: 50,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MinigamesApp()),
-                    );
-                  },
-                ),
-                IconButton(
-                  icon:const Icon(Icons.shop),
-                  iconSize: 50,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ShopApp()),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
