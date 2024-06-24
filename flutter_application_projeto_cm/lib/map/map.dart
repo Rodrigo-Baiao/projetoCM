15a4741999badc3592db020821ecd735495c284a
isidoroforo <202200147@estudantes.ips.pt>
2024-06-24 02:17:27 +0100

    localização utilizador

--- flutter_application_projeto_cm/lib/map/map.dart
+++ flutter_application_projeto_cm/lib/map/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -8,11 +9,80 @@ class MapScreen extends StatefulWidget {
 
 class MapScreenState extends State<MapScreen> {
   GoogleMapController? mapController;
+  Location _location = Location();
 
-  final LatLng _center = const LatLng(37.7749, -122.4194); // San Francisco coordinates
+  LatLng _initialPosition = LatLng(-23.5505, -46.6333); // São Paulo
+  LatLng _currentPosition = LatLng(-23.5505, -46.6333); // Posição inicial
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _initLocation();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    _location.onLocationChanged.listen((LocationData currentLocation) {
+      setState(() {
+        _currentPosition = LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        _updateMarker(_currentPosition);
+        _centerMapOnCurrentPosition();
+      });
+    });
+  }
+
+  Future<void> _initLocation() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    // Verifica se o serviço de localização está habilitado.
+    _serviceEnabled = await _location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    // Verifica se a permissão de localização foi concedida.
+    _permissionGranted = await _location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await _location.getLocation();
+    setState(() {
+      _currentPosition = LatLng(_locationData.latitude!, _locationData.longitude!);
+      _updateMarker(_currentPosition);
+    });
+    _centerMapOnCurrentPosition();
+  }
+
+  void _updateMarker(LatLng position) {
+    setState(() {
+      _markers.clear();
+      _markers.add(Marker(
+        markerId: MarkerId('currentLocation'),
+        position: position,
+        infoWindow: InfoWindow(title: 'Você está aqui'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+      ));
+    });
+  }
+
+  void _centerMapOnCurrentPosition() {
+    if (mapController != null) {
+      mapController!.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: _currentPosition, zoom: 15.0),
+        ),
+      );
+    }
   }
 
   @override
@@ -20,9 +90,12 @@ class MapScreenState extends State<MapScreen> {
     return GoogleMap(
       onMapCreated: _onMapCreated,
       initialCameraPosition: CameraPosition(
-        target: _center,
+        target: _initialPosition,
         zoom: 11.0,
       ),
+      myLocationEnabled: true,
+      myLocationButtonEnabled: true,
+      markers: _markers,
     );
   }
 }
